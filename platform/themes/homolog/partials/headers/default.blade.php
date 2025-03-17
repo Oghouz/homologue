609d780c8495286e4b7912befe3ae5b6b68bc164
Maimaiti <maimaiti@pharmaland.fr>
2025-03-17 14:38:50 +0100

    suite

--- platform/themes/homolog/partials/headers/default.blade.php
+++ platform/themes/homolog/partials/headers/default.blade.php
@@ -14,14 +14,13 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-4">
+                <div class="col-xl-4 col-lg-4 logo-container">
                     @if($logo = theme_option('logo'))
-                        <div class="text-center" style="transition: font-size 0.5s" id="logo">
-                            <a href="{{ route('public.index') }}" id="home_page_logo_text" style="color: white">
-                                <sapn style="font-size: 4.5em;font-family: emoji;">HOMOLOG</sapn>
+                        <div class="text-center logo-overlay">
+                            <a href="{{ route('public.index') }}" id="home_page_logo_text">
+                                <sapn style="font-size: 4.2em;font-family: emoji;">HOMOLOG</sapn>
                                 <hr style="color: transparent;margin: 5px 0;">
                                 <span style="font-size: 2.3em;font-family: emoji;">PARIS</span>
-{{--                                    {{ RvMedia::image($logo, theme_option('site_title')) }}--}}
                             </a>
                         </div>
                     @endif
@@ -29,15 +28,6 @@
                 @if (is_plugin_active('ecommerce'))
                     <div class="col-xl-4 col-lg-4">
                         <div class="header-meta justify-content-end">
-{{--                            <div class="mainmenu__search w-100">--}}
-{{--                                <form action="{{ route('public.products') }}" class="position-relative form--quick-search" data-url="{{ route('public.ajax.search-products') }}" method="GET">--}}
-{{--                                    <div class="mainmenu__search-bar p-relative">--}}
-{{--                                        <button class="mainmenu__search-icon" title="search"><i class="fal fa-search"></i></button>--}}
-{{--                                        <input type="text" name="q" class="input-search-product" placeholder="{{ __('Search products...') }}" value="{{ BaseHelper::stringify(request()->query('q')) }}" autocomplete="off">--}}
-{{--                                    </div>--}}
-{{--                                    <div class="panel--search-result"></div>--}}
-{{--                                </form>--}}
-{{--                            </div>--}}
                             {!! Theme::partial('header-meta') !!}
                         </div>
                     </div>
--- platform/themes/homolog/partials/menu.blade.php
+++ platform/themes/homolog/partials/menu.blade.php
@@ -1,4 +1,54 @@
-<ul {!! $options !!}>
+<div class="menu-icon" onclick="toggleMenu()">
+    <i class="fal fa-bars"></i>
+</div>
+
+<div class="menu-overlay">
+    <div class="row">
+        <div class="col-xl-4 col-lg-4">
+            <ul id="nav-left">
+                @foreach ($menu_nodes as $key => $node)
+                @php($hasMegaMenu = $node->has_child && count($node->child) > 12)
+                <li>
+                    <a href="{{ url($node->url) }}" @if ($node->target !== '_self') target="{{ $node->target }}" @endif
+                        class="{{ $hasMegaMenu ? 'mega-menu-title' : '' }}">
+                        {{ $node->title }}
+                    </a>
+
+                    @if ($node->has_child)
+                        <ul class="submenu">
+                            @foreach ($node->child as $child)
+                                <li>
+                                    <a href="{{ url($child->url) }}" @if ($child->target !== '_self')
+                                    target="{{ $child->target }}" @endif>
+                                        {{ $child->title }}
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </li>
+                @endforeach
+            </ul>
+        </div>
+
+        <div class="col-xl-4 col-lg-4 text-center">
+            <img src="http://homolog.test/storage/2024/menu-img.jpg" alt="Logo" width="100" height="100">
+        </div>
+
+        <div class="col-xl-4 col-lg-4 text-end">
+            <ul>
+                <li><a href="#">Recherche</a></li>
+                <li><a href="#">Compte</a></li>
+                <li><a href="#">Ma Sélection</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+
+
+{{-- <ul {!! $options !!}>
     @foreach ($menu_nodes as $key => $node)
         @php($hasMegaMenu = $node->has_child && count($node->child) > 12)
         <li @if($node->has_child) onclick="openNavMenu('{{ $node->title }}', '{{ $node->id }}')" @endif>
@@ -9,23 +59,14 @@
             @if ($node->has_child)
                 @if ($hasMegaMenu)
                     {!! Menu::generateMenu([
-                       'menu' => $node,
-                       'menu_nodes' => $node->child,
-                       'view' => 'mega-menu',
-                       'options' => ['class' => 'submenu mega-menu'],
-                   ]) !!}
-                @else
-{{--                    {!! Menu::generateMenu([--}}
-{{--                        'menu' => $node,--}}
-{{--                        'menu_nodes' => $node->child,--}}
-{{--                        'view' => 'menu',--}}
-{{--                        'options' => ['class' => 'submenu'],--}}
-{{--                    ]) !!}--}}
-                @endif
-            @endif
+                'menu' => $node,
+                'menu_nodes' => $node->child,
+                'view' => 'mega-menu',
+                'options' => ['class' => 'submenu mega-menu'],
+            ]) !!}
         </li>
     @endforeach
-</ul>
+</ul> --}}
 <div id="navMenuSidebar" class="nav-menu-sidebar">
     <div class="nav-menu-content">
         <div class="text-end mr-25">
